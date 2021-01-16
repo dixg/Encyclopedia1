17ab465666d85e9b29b10b83f3dcbce64948bfe4
Dixika Grewal <dixikagrewal@Dixikas-MacBook-Pro.local>
2021-01-16 12:53:43 +0530

    new edits

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.display_entry),
-    path("?q<str:entry>", views.search)
+    path("wiki/<str:entry>", views.display_entry, name="display"),
+    path("search", views.search, name="search"),
+    path("newPage", views.newPage, name="newPage")
 ]
